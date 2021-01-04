51ecafd511ceee24442cd9585cab3f212ab1e53a
Leroy Salih <leroy.salih@gmail.com>
2021-01-04 03:23:53 +0000

    Adding GET command

--- utils/get.py
+++ utils/get.py
@@ -1,6 +1,12 @@
-
-
+import requests
+import json
 
 if __name__ =="__main__":
   print("Getting Files from DB.")
+
+  r = requests.get('https://cs-revise.leroysalih.vercel.app/api/challenge/1', data = {'main':'#Written from gitpod'})
+  obj = json.loads(r.text)
+  print(obj)
+  print(obj['main'])
+  print(obj['test'])
 
